f62eaee03350d3f6c2c0adff5952aff014868b64
Durai Ramasubramanian <durairam89@gmail.com>
2020-12-24 13:06:11 +0000

    Update FindNthNodeLinkedList.java

--- src/main/java/com/durai/competitive_problems/hackerrank/FindNthNodeLinkedList.java
+++ src/main/java/com/durai/competitive_problems/hackerrank/FindNthNodeLinkedList.java
@@ -2,24 +2,23 @@ import java.util.*;
 
 class Program {
   public static void removeKthNodeFromEnd(LinkedList head, int k) {
-  	 	LinkedList fastPointer = head;
-		  LinkedList slowPointer = head;
-			int count = 1; 
-			while (count <= k) {
-				fastPointer = fastPointer.next;
-				count++;
-			}
-			if (fastPointer == null) {
-				head.value = head.next.value;
-				head.next = head.next.next;
-				return;
-			}
-		
-		  while (fastPointer.next != null) {
-				slowPointer = slowPointer.next;
-				fastPointer = fastPointer.next;
-			}
-		  slowPointer.next =  slowPointer.next.next;
+  	LinkedList fastPointer = head;
+	LinkedList slowPointer = head;
+	int count = 1; 
+	while (count <= k) {
+	  fastPointer = fastPointer.next;
+	  count++;
+	}
+	if(fastPointer == null) {
+	  head.value = head.next.value;
+	  head.next = head.next.next;
+	  return;
+	}
+	while(fastPointer.next != null) {
+	  slowPointer = slowPointer.next;
+	  fastPointer = fastPointer.next;
+	}
+	slowPointer.next =  slowPointer.next.next;
   }
 
   static class LinkedList {
