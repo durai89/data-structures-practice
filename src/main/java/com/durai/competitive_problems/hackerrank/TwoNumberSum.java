01fb53d1b4c960955fd527cabdbf64041c661f8f
Durai Ramasubramanian <durairam89@gmail.com>
2020-12-26 21:45:12 +0000

    Update TwoNumberSum.java

--- src/main/java/com/durai/competitive_problems/hackerrank/TwoNumberSum.java
+++ src/main/java/com/durai/competitive_problems/hackerrank/TwoNumberSum.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 class Program {
   public static int[] twoNumberSum(int[] array, int targetSum) {
-  	Set<Integer> numberSet = new HashSet<>();
-		for (int i=0; i < array.length; i++) {
-			if (numberSet.contains(targetSum - array[i])) {
-				return new int[]{array[i], targetSum - array[i]};
-			}
-			numberSet.add(array[i]);
-		}
-    return new int[0];
+     Set<Integer> numberSet = new HashSet<>();
+     for (int i=0; i < array.length; i++) {
+	if(numberSet.contains(targetSum - array[i])) {
+	   return new int[]{array[i], targetSum - array[i]};
+	}
+	numberSet.add(array[i]);
+     }
+     return new int[0];
   }
 }
