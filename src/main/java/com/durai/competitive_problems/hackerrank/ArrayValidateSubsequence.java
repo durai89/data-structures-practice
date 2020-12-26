6b616b33df05d66a664c8a85c2e5c681d806070a
Durai Ramasubramanian <durairam89@gmail.com>
2020-12-26 22:37:21 +0000

    Update ArrayValidateSubsequence.java

--- src/main/java/com/durai/competitive_problems/hackerrank/ArrayValidateSubsequence.java
+++ src/main/java/com/durai/competitive_problems/hackerrank/ArrayValidateSubsequence.java
@@ -3,14 +3,14 @@ import java.util.*;
 class Program {
   public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
     int i = 0, j = 0;
-		while (i < array.size() && j < sequence.size()) {
-			 if (array.get(i) == sequence.get(j)) {
-				  j++;
-			 } else {
-				  i++;
-			 }
-			 i++;
-		}
-		return j == sequence.size();
+    while (i < array.size() && j < sequence.size()) {
+      if(array.get(i) == sequence.get(j)) {
+	j++;
+      } else {
+	i++;
+      }
+      i++;
+    }
+    return j == sequence.size();
   }
 }
