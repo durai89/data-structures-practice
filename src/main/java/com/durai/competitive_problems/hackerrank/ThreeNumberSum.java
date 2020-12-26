341e461f5b28c6582b335a83832c60c8bdb5c3ca
Durai Ramasubramanian <durairam89@gmail.com>
2020-12-26 23:14:08 +0000

    Update ThreeNumberSum.java

--- src/main/java/com/durai/competitive_problems/hackerrank/ThreeNumberSum.java
+++ src/main/java/com/durai/competitive_problems/hackerrank/ThreeNumberSum.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Program {
   public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
-		List<Integer[]> output = new LinkedList<>();
-		int l, h, sum = 0;
-		Arrays.sort(array); 
-   	for (int i=0; i < array.length; i++) {
-				l = i + 1;
-				h = array.length - 1;
-  			while (h - l > 0) {
-					sum = array[i] + array[l] + array[h]; 
-	  			if (sum == targetSum) {
-						output.add(new Integer[]{array[i], array[l], array[h]});
-						l++;
-					  h--;
-					}
-					else if (sum < targetSum) {
-						l++;
-					} else {
- 						h--;
-					}
-				}
-		}
-		return output;
+     List<Integer[]> output = new LinkedList<>();
+     int l, h, sum = 0;
+     Arrays.sort(array); 
+     for(int i=0; i < array.length; i++) {
+	l = i + 1;
+	h = array.length - 1;
+  	while(h - l > 0) {
+	  sum = array[i] + array[l] + array[h]; 
+	  if(sum == targetSum) {
+	    output.add(new Integer[]{array[i], array[l], array[h]});
+	    l++;
+	    h--;
+	  }
+	  else if(sum < targetSum) {
+	    l++;
+	  } else {
+ 	    h--;
+	  }
+	}
+     }
+     return output;
   }
 }
