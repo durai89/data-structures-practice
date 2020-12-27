00cdae09328a986a617e30a3f556f76b8f742618
Durai Ramasubramanian <durairam89@gmail.com>
2020-12-27 01:09:04 +0000

    Update FirstDuplicateValue.java

--- src/main/java/com/durai/competitive_problems/hackerrank/FirstDuplicateValue.java
+++ src/main/java/com/durai/competitive_problems/hackerrank/FirstDuplicateValue.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
 class Program {
+	
   public int firstDuplicateValue(int[] array) {
-    for (int value: array) {
-			int index = Math.abs(array[i]) - 1;
-			if (array[index] < 0) {
-				return index + 1;
-			}
-			array[index] = -1 * array[index];   
-		} 
-    return -1;
+      for(int value: array) {
+         int index = Math.abs(value) - 1;
+         if (array[index] < 0) {
+	   return index + 1;
+         }
+         array[index] = -1 * array[index];
+      }
+      return -1;
   }
 }
