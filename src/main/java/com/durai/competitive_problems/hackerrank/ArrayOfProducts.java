81be9c2f9fb4d1387d7ac03369259b4a5791a661
Durai Ramasubramanian <durairam89@gmail.com>
2020-12-28 20:06:00 +0000

    Update ArrayOfProducts.java

--- src/main/java/com/durai/competitive_problems/hackerrank/ArrayOfProducts.java
+++ src/main/java/com/durai/competitive_problems/hackerrank/ArrayOfProducts.java
@@ -3,19 +3,18 @@ import java.util.*;
 class Program {
   public int[] arrayOfProducts(int[] array) {
     int[] product = new int[array.length];
-		
-		int leftRunningProduct = 1;
-		for (int i=0; i < array.length; i++) {
-			product[i]	= leftRunningProduct;
-			leftRunningProduct *= array[i];
-		}
+    
+    int leftRunningProduct = 1;
+    for(int i=0; i < array.length; i++) {
+       product[i] = leftRunningProduct;
+       leftRunningProduct *= array[i];
+    }
  
-		int rightRunningProduct = 1;
-		for (int i=array.length - 1; i >= 0; i--) {
-			product[i]	*= rightRunningProduct;
-			rightRunningProduct *= array[i];
-		}
-		
+    int rightRunningProduct = 1;
+    for(int i=array.length - 1; i >= 0; i--) {
+       product[i] *= rightRunningProduct;
+       rightRunningProduct *= array[i];
+    }	
     return product;
   }
 }
