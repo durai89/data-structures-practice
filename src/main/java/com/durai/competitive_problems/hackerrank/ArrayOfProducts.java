c8a916e97e614f30f484bd976e088585bd4871c1
Durai Ramasubramanian <durairam89@gmail.com>
2020-12-28 20:03:44 +0000

    Create ArrayOfProducts.java

---
+++ src/main/java/com/durai/competitive_problems/hackerrank/ArrayOfProducts.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Program {
+  public int[] arrayOfProducts(int[] array) {
+    int[] product = new int[array.length];
+		
+		int leftRunningProduct = 1;
+		for (int i=0; i < array.length; i++) {
+			product[i]	= leftRunningProduct;
+			leftRunningProduct *= array[i];
+		}
+ 
+		int rightRunningProduct = 1;
+		for (int i=array.length - 1; i >= 0; i--) {
+			product[i]	*= rightRunningProduct;
+			rightRunningProduct *= array[i];
+		}
+		
+    return product;
+  }
+}
